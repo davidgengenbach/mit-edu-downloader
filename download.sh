0a26ff9fd3ad44da675758f89bb0755a7f0ca647
David Gengenbach <davidgengenbach@gmx.de>
2016-10-08 23:15:53 +0200

    :)

--- download.sh
+++ download.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+URL=$1
 OUT=youtube_urls.txt
-./download.py > $OUT
+
+if [ -z $URL ]; then
+    echo "Usage: $0 url"
+    exit 1
+fi
+
+./download.py $URL > $OUT
 youtube-dl -f bestvideo+bestaudio -a $OUT
